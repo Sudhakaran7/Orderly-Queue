896206ed858d22a4443515071ae29b61d4a74eec
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-09-21 08:40:17 +0530

    Create order.py

---
+++ order.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def orderlyQueue(self, S, K):
+        """
+        :type S: str
+        :type K: int
+        :rtype: str
+        """
+        if K == 1:
+            return min(S[i:] + S[:i] for i in range(len(S)))
+        return "".join(sorted(S))
+
+val=Solution()
+s,k=input().split()
+print(val.orderlyQueue(s,int(k)))      
